cf2b31de4efe24ee60f00d0952db336def668e74
Aleksandar <aleksandarbojovic8@gmail.com>
2020-02-18 00:00:19 +0100

    first commit

---
+++ WorkingWithExpress/app.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () => console.log('Server is working'));
