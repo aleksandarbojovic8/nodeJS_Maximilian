7141ca514a392b206194bd0e20b02b5b64d6d71b
Aleksandar <aleksandarbojovic8@gmail.com>
2020-02-21 01:03:42 +0100

    working with express module final

--- WorkingWithExpress/app.js
+++ WorkingWithExpress/app.js
@@ -9,12 +9,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000, () => console.log('Server is working'));
